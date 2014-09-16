7f2dc0440aac2d5f14e26139d5856f5db33e0ceb
Plamen Tanov <ptanov@setelis.com>
2014-09-16 18:34:31 +0300

    First lesson - variables

---
+++ 01_SampleJava/src/org/elsysbg/ip/java/Fundamentals.java
@@ -0,0 +1,25 @@
+package org.elsysbg.ip.java;
+
+public class Fundamentals {
+	// to create constant
+	private final static String SOME_CONSTANT_VALUE = "constant1";
+	
+	// to create field
+	private String c = "other";
+	
+	public static void main(String[] args) {
+		// to output to console
+		System.out.println("Hello world");
+
+		// to create local variable
+		String c;
+		// to create local variable with initial value
+		int a = 1;
+		String b = "something";
+
+		// to assign value to variable
+		c = b + a;
+		System.out.println(c);
+	}
+
+}
