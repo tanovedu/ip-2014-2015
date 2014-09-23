4f3ba994336e41c5cb90844f2ab881637284be3e
Plamen Tanov <ptanov@setelis.com>
2014-09-23 16:42:06 +0300

    Working with methods

--- 01_SampleJava/src/org/elsysbg/ip/java/Fundamentals.java
+++ 01_SampleJava/src/org/elsysbg/ip/java/Fundamentals.java
@@ -20,6 +20,46 @@ public class Fundamentals {
 		// to assign value to variable
 		c = b + a;
 		System.out.println(c);
+		
+		// how to create object and assign to variable
+		Fundamentals fundamentals = new Fundamentals();
+		fundamentals = new Fundamentals();
+		
+		// final keyword
+		final Fundamentals fundamentalsFinal = new Fundamentals();
+		// won't work
+		// fundamentalsFinal = new Fundamentals();
+		
+		// how to call method
+		fundamentalsFinal.getGreeting();
+		
+		// how to get result of method invocation
+		final String result = fundamentalsFinal.getGreeting();
+		
+		System.out.println(result);
+		
+//		won't work - no value
+//		String a = 
+		fundamentalsFinal.doSomething();
+		
+		// how to pass arguments to a method
+		final String greetingBulgaria = fundamentalsFinal.getGreeting("Bulgaria");
+		System.out.println(greetingBulgaria);
+	}
+	
+	// how to define method:
+	public String getGreeting() {
+		return "Hello world";
+	}
+	
+	// how to define method without result
+	public void doSomething() {
+		// nothing
+	}
+	
+	// how to pass arguments to a method
+	public String getGreeting(String name) {
+		return "Hello " + name;
 	}
 
 }
