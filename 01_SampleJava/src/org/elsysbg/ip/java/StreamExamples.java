ad28e8ceabbf603bbc90f54a32de6e8a52f1c9c4
Plamen Tanov <ptanov@setelis.com>
2014-09-23 18:01:06 +0300

    BufferedReader Example

--- 01_SampleJava/src/org/elsysbg/ip/java/StreamExamples.java
+++ 01_SampleJava/src/org/elsysbg/ip/java/StreamExamples.java
@@ -1,14 +1,36 @@
 package org.elsysbg.ip.java;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class StreamExamples {
 
+	private static final String END_MARKER = "END";
+
 	public static void main(String[] args) throws IOException {
-		inputStreamExample();
+//		inputStreamExample();
+		bufferedReaderExample();
 	}
 	
+	private static void bufferedReaderExample() throws IOException {
+		final InputStream input = System.in;
+		final InputStreamReader inputStreamReader = new InputStreamReader(input);
+		final BufferedReader reader = new BufferedReader(inputStreamReader);
+		
+		// to read whole lien:
+		final String wholeLine = reader.readLine();
+		System.out.println(wholeLine);
+		
+		// read until keyword
+		int count = 0;
+		while(!reader.readLine().equals(END_MARKER)) {
+			count ++;
+		}
+		System.out.println("You have entered " + count);
+	}
+
 	// rethrow exception with throws
 	public static void inputStreamExample() throws IOException {
 		final InputStream input = System.in;
