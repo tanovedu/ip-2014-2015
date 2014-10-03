1a01a1d2f361ce6555741f5254fbf215f0e13d01
Plamen Tanov <ptanov@setelis.com>
2014-10-03 19:14:23 +0300

    Sample echo server for multiple clients

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
@@ -12,13 +12,57 @@ import java.net.Socket;
 public class EchoServer {
 
 	private static final int SERVER_PORT = 44012;
+	
+	// if static - ClientHandler will not have access to EchoServer.started
+	// so server can be stopped if "END" command is implemented in client handler
+	
+	// extends Thread - create class that can be spawned in new thread
+	private /*static*/ class ClientHandler extends Thread {
+		private final Socket clientSocket;
+		
+		public ClientHandler(Socket clientSocket) {
+			this.clientSocket = clientSocket;
+		}
 
+		@Override
+		public void run() {
+			try {
+				handleClient(clientSocket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		private void handleClient(final Socket clientSocket) throws IOException {
+			//get I/O streams
+			final InputStream inputStream = clientSocket.getInputStream();
+			final OutputStream outputStream = clientSocket.getOutputStream();
+			
+			final InputStreamReader inputStreamReader =
+				new InputStreamReader(inputStream);
+			final BufferedReader in = new BufferedReader(inputStreamReader);
+			
+			final PrintWriter out = new PrintWriter(outputStream);
+			
+			// read from socket
+			final String readLine = in.readLine();
+			
+			// write to socket what we have red (this is echo server)
+			out.println(readLine);
+			
+			// always flush writer
+			out.flush();
+			
+			// we should ALWAYS close sockets!
+			clientSocket.close();
+		}
+		
+	}
+	
 	private boolean started;
 
 	public static void main(String[] args) throws IOException {
 		new EchoServer().run();
 	}
-
 	
 	public void run() throws IOException {
 		started = true;
@@ -30,35 +74,13 @@ public class EchoServer {
 			// accept client
 			final Socket clientSocket = serverSocket.accept();
 
-			handleClient(clientSocket);
+			//Thread.start() creates new thread and calls run() method
+			new ClientHandler(clientSocket).start();
 		}
 		
 		// we should ALWAYS close sockets!
 		serverSocket.close();
 	}
 
-	private void handleClient(final Socket clientSocket) throws IOException {
-		//get I/O streams
-		final InputStream inputStream = clientSocket.getInputStream();
-		final OutputStream outputStream = clientSocket.getOutputStream();
-		
-		final InputStreamReader inputStreamReader =
-			new InputStreamReader(inputStream);
-		final BufferedReader in = new BufferedReader(inputStreamReader);
-		
-		final PrintWriter out = new PrintWriter(outputStream);
-		
-		// read from socket
-		final String readLine = in.readLine();
-		
-		// write to socket what we have red (this is echo server)
-		out.println(readLine);
-		
-		// always flush writer
-		out.flush();
-		
-		// we should ALWAYS close sockets!
-		clientSocket.close();
-	}
 
 }
