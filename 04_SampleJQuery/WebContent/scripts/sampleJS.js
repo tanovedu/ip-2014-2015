0dd3bb22e6dcc2e2d4c77c57c4096a62b0ef1d17
Plamen Tanov <tanovedu@gmail.com>
2014-11-28 18:11:54 +0200

    Examples for using variables

--- 04_SampleJQuery/WebContent/scripts/sampleJS.js
+++ 04_SampleJQuery/WebContent/scripts/sampleJS.js
@@ -3,4 +3,17 @@
 	
 	// display alert on the screen
 	alert("Hello world");
+	
+	// define variable
+	var i;
+	
+	// assign value
+	i = 1;
+	// increase value
+	i++;
+	
+	if (i === 2) {
+		alert("TWO");
+	}
+	// for, while
 })();
