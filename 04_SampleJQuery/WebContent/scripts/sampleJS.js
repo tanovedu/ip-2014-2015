cbb014ac43a481086f3914ae679acec5c490d48d
Plamen Tanov <tanovedu@gmail.com>
2014-11-28 18:14:41 +0200

    Examples for using objects

--- 04_SampleJQuery/WebContent/scripts/sampleJS.js
+++ 04_SampleJQuery/WebContent/scripts/sampleJS.js
@@ -26,4 +26,29 @@
 	// access element of an array by index 
 	alert(array[0]);
 
+	// define object:
+	var o = {
+		some: "value"	
+	};
+	// access field:
+	alert(o.some);
+	// change field value:
+	o.some = "value2";
+	// dynamically add fields:
+	o.newField = "newValue";
+	alert(o.newField);
+	
+	// dynamic name of field
+	o[o.newField] = "dynamic name";
+	alert(o.newValue);
+	
+	// objects are passed by reference
+	var otherObject = {};
+	// object can contains other objects
+	o.subObject = otherObject;
+	o.subObject.id = 1;
+	if (o.subObject.id === otherObject.id) {
+		alert("TRUE");
+	}
+	
 })();
