1370a42449dca6c78bf8da9d3cad579db84c85fd
Plamen Tanov <tanovedu@gmail.com>
2014-12-12 18:46:16 +0200

    Example for iteration over first 5 elements

--- 04_SampleJQuery/WebContent/scripts/sampleAjax.js
+++ 04_SampleJQuery/WebContent/scripts/sampleAjax.js
@@ -11,12 +11,23 @@ $(document).ready(function() {
 		var list = $("#list1");
 		//iterating over array, something like:
 		//for(var i=0; i<response.length; i++)
+		
+		// iterate over first 5 elements:		
+		//$.each(response.slice(0, 5), function() {
+		
+		var i = 0;
 		$.each(response, function() {
 			// next element is "this"
 			// response[i] is "this"
 			// console.log(this);
 			
 			appendToList(list, this);
+			// iterate over first 5 elements:		
+			if (++i >= 5) {
+				// when in $.each() function returns false
+				// loop is canceled
+				return false;
+			}
 		});
 	}
 	
