f021498ce650fe48e5dda8db63a7b278827b231e
Plamen Tanov <tanovedu@gmail.com>
2014-12-12 18:19:28 +0200

    Example POST with request body

--- 04_SampleJQuery/WebContent/scripts/sampleAjax.js
+++ 04_SampleJQuery/WebContent/scripts/sampleAjax.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
 	"use strict"
 
 	// execute GET request to given url
-	$.ajax("http://jsonplaceholder.typicode.com/posts/1", {
-	  method: "GET"
+	$.ajax("http://jsonplaceholder.typicode.com/posts", {
+	  method: "POST",
+	  // add request body as JSON:
+	  data: {
+		  userId: 1,
+		  body: "newContent",
+		  title: "newPost"
+	  }
 	// function in then() is callback that will be called
 	// when response is received
 	}).then(function(data) {
