af641cda170ff583444cf162cdd993caa48a7484
Plamen Tanov <tanovedu@gmail.com>
2014-12-12 17:39:56 +0200

    Example for creating element

--- 04_SampleJQuery/WebContent/scripts/sampleJQuery.js
+++ 04_SampleJQuery/WebContent/scripts/sampleJQuery.js
@@ -31,6 +31,18 @@ $(document).ready(function() {
 	$("li.pretty").on("mouseenter", function() {
 		console.log("mouse entered the element");
 	});
+	
+	//creating element
+	var newListItem = $("<li/>");
+	// setting text of element
+	newListItem.text("new");
+	// newListItem.append(someOtherElement);
+	// add attribute
+	newListItem.attr("id", new Date().getTime());
+	// append child
+	list.append(newListItem);
+	// equal to
+	// list.append($("<li/>").text("new2").attr("id", new Date().getTime()));
 });
 /* shorthand:
 $(function() {
