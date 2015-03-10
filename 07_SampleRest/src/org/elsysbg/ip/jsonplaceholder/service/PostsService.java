3cbbc615671c0f558061da0ce83c5f04c2ea96c6
Plamen Tanov <tanovedu@gmail.com>
2015-03-10 08:44:27 +0200

    Sample post service implemented

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/service/PostsService.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/service/PostsService.java
@@ -1,25 +1,48 @@
 package org.elsysbg.ip.jsonplaceholder.service;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.elsysbg.ip.jsonplaceholder.model.Post;
 
+/**
+ * TODO write tests for posts service
+ */
 public class PostsService {
 
+	private final List<Post> posts =
+			new LinkedList<Post>();
+	
+	private long lastPostId = 0;
+
 	public List<Post> getPosts() {
-		return null;
+		return posts;
 	}
+	
 	public Post getPost(long postId) {
+		for (Post next : posts) {
+			if (next.getId() == postId) {
+				return next;
+			}
+		}
 		return null;
 	}
-	public Post createPost(Post post) {
-		return null;
+	// synchronized because of lastPostId
+	public synchronized Post createPost(Post post) {
+		lastPostId++;
+		// will be done in DB
+		post.setId(lastPostId);
+		posts.add(post);
+		return post;
 	}
 	public Post updatePost(Post post) {
-		return null;
+		deletePost(post.getId());
+		posts.add(post);
+		return post;
 	}
 	public void deletePost(long postId) {
-		
+		final Post toBeDeleted = getPost(postId);
+		posts.remove(toBeDeleted);
 	}
 
 }
