b55130bf2739cc1053b98b20439bca50d4fa4190
Plamen Tanov <tanovedu@gmail.com>
2015-03-10 09:01:44 +0200

    Added sample test

--- 07_SampleRest/test/org/elsysbg/ip/jsonplaceholder/service/PostsServiceTest.java
+++ 07_SampleRest/test/org/elsysbg/ip/jsonplaceholder/service/PostsServiceTest.java
@@ -1,36 +1,42 @@
 package org.elsysbg.ip.jsonplaceholder.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import org.elsysbg.ip.jsonplaceholder.model.Post;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * For unit testing you can use JUnit or TestNG
  */
 public class PostsServiceTest {
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	private PostsService postsService;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		postsService = new PostsService();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateGetPost() {
+// not set as required in post model
+//		final User author = new User();
+//		author.setEmail("hello@world");
+//		author.setPassword("secret");
+		final Post post = new Post();
+		post.setTitle("hello");
+		post.setBody("world");
+//		post.setUser(author);
+		final Post result =
+			postsService.createPost(post);
+		assertNotEquals(0, result.getId());
+		
+		final Post fromService =
+			postsService.getPost(post.getId());
+		assertEquals("hello", fromService.getTitle());
+		assertEquals("world", fromService.getBody());
 	}
-
+	
+	// TODO add more tests (update, delete, increment ID, etc.)
 }
