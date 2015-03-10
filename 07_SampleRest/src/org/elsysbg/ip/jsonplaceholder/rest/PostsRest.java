56fdcd2e77ea9d22b38934b06a8fd9dc4c88989d
Plamen Tanov <tanovedu@gmail.com>
2015-03-10 09:07:34 +0200

    Bad implementation of PostsRest

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/rest/PostsRest.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/rest/PostsRest.java
@@ -3,22 +3,36 @@ package org.elsysbg.ip.jsonplaceholder.rest;
 import java.util.List;
 
 import org.elsysbg.ip.jsonplaceholder.model.Post;
+import org.elsysbg.ip.jsonplaceholder.model.User;
+import org.elsysbg.ip.jsonplaceholder.service.PostsService;
 
 public class PostsRest {
+	private final PostsService postsService;
+	private final User defaultAuthor;
+	public PostsRest() {
+		postsService = new PostsService();
+		
+		// TODO should be get from session
+		defaultAuthor = new User();
+		defaultAuthor.setEmail("hello@world");
+		defaultAuthor.setPassword("secret");
+	}
 
 	public List<Post> getPosts() {
-		return null;
+		return postsService.getPosts();
 	}
 	public Post getPost(long postId) {
-		return null;
+		return postsService.getPost(postId);
 	}
 	public Post createPost(Post post) {
-		return null;
+		// TODO set author by user session
+		post.setUser(defaultAuthor);
+		return postsService.createPost(post);
 	}
 	public Post updatePost(Post post) {
-		return null;
+		return postsService.updatePost(post);
 	}
 	public void deletePost(long postId) {
-		
+		postsService.deletePost(postId);
 	}
 }
