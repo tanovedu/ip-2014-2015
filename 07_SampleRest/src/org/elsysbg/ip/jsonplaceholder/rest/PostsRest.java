abcf3eabbddf1a710d05bc1960e8287d8075383c
Plamen Tanov <tanovedu@gmail.com>
2015-03-10 08:32:44 +0200

    Endpoint template added

---
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/rest/PostsRest.java
@@ -0,0 +1,24 @@
+package org.elsysbg.ip.jsonplaceholder.rest;
+
+import java.util.List;
+
+import org.elsysbg.ip.jsonplaceholder.model.Post;
+
+public class PostsRest {
+
+	public List<Post> getPosts() {
+		return null;
+	}
+	public Post getPost(long postId) {
+		return null;
+	}
+	public Post createPost(Post post) {
+		return null;
+	}
+	public Post updatePost(Post post) {
+		return null;
+	}
+	public void deletePost(long postId) {
+		
+	}
+}
