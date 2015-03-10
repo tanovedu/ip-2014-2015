b7a9cd20d7649d04dd608b9d4dc538c9d912e81f
Plamen Tanov <tanovedu@gmail.com>
2015-03-10 08:34:00 +0200

    Service template added

---
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/service/PostsService.java
@@ -0,0 +1,25 @@
+package org.elsysbg.ip.jsonplaceholder.service;
+
+import java.util.List;
+
+import org.elsysbg.ip.jsonplaceholder.model.Post;
+
+public class PostsService {
+
+	public List<Post> getPosts() {
+		return null;
+	}
+	public Post getPost(long postId) {
+		return null;
+	}
+	public Post createPost(Post post) {
+		return null;
+	}
+	public Post updatePost(Post post) {
+		return null;
+	}
+	public void deletePost(long postId) {
+		
+	}
+
+}
